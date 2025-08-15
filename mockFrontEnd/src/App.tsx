ee6e081205c71457167ec1aeffb18a95834a065c
Endiviour <thesoder1@gmail.com>
2025-08-15 14:02:12 +0700

    Remove BrowserRouter wrapper from App component

--- mockFrontEnd/src/App.tsx
+++ mockFrontEnd/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./Modules/Homepage/components/Home";
 import About from "./Modules/Homepage/components/About";
 import Contact from "./Modules/Homepage/components/Contact";
@@ -7,15 +7,12 @@ import { DoctorPage } from "./Modules/Doctor/components";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/doctors" element={<DoctorPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/doctors" element={<DoctorPage />} />
+    </Routes>
   );
 }
 export default App;
-
